7afa74d335adcd3f7f50ded8f18495760b2a47d5
Ironykins <konrad.aust@gmail.com>
2015-11-03 20:29:00 -0700

    Initial commit of scheduling repository. Just a hello world program currently.

---
+++ CourseScheduler/src/scheduler/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package scheduler;
+
+/**
+ * Main Class
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args Command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
