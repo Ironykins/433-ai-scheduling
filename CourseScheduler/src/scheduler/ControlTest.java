b217fcb567ae4b4f77fa6976d9d5bb5433169117
Mark Barley <mbarley66@gmail.com>
2015-12-05 14:46:10 -0700

    added test file for control, removed some test stuff

---
+++ CourseScheduler/src/scheduler/ControlTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package scheduler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author mark
+ *
+ */
+public class ControlTest {
+
+	/**
+	 * Test method for {@link scheduler.Control#Control(scheduler.Problem)}.
+	 */
+	@Test
+	public void testControl() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link scheduler.Control#solve()}.
+	 */
+	@Test
+	public void testSolve() {
+		fail("Not yet implemented");
+	}
+
+}
